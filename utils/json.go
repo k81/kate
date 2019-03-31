ba757a480ed36a5c40f0695a6b9ed7643b89de23
kaizhangkai <kaizhangkai@didichuxing.com>
2019-03-31 21:28:23 +0800

    update

--- utils/json.go
+++ utils/json.go
@@ -1,9 +1,9 @@
 package utils
 
 import (
-	"bytes"
 	"encoding/json"
-	"reflect"
+
+	"github.com/k81/dynamicjson"
 )
 
 // ToJSON return json encoded string of interface.
@@ -24,86 +24,7 @@ type DynamicFielder interface {
 	NewDynamicField(fieldName string) interface{}
 }
 
-// getInnerPtrValue
-// val: *struct{}
-// ind: struct{}
-func getInnerPtrValue(ptr interface{}) (val, ind reflect.Value) {
-	val = reflect.ValueOf(ptr)
-	ind = reflect.Indirect(val)
-
-	for {
-		switch ind.Kind() {
-		case reflect.Interface:
-			fallthrough
-		case reflect.Ptr:
-			if ind.IsNil() {
-				ind.Set(reflect.New(ind.Type().Elem()))
-			}
-			val = ind
-			ind = val.Elem()
-		default:
-			return val, ind
-		}
-	}
-}
-
-type dynField struct {
-	Name  string
-	Value *reflect.Value
-}
-
 // ParseJSON parse json with dynamic field parse support
 func ParseJSON(data []byte, ptr interface{}) error {
-	data = bytes.TrimSpace(data)
-	if len(data) == 0 {
-		// ignore empty field
-		return nil
-	}
-
-	val, ind := getInnerPtrValue(ptr)
-	ptr = val.Interface()
-
-	if ind.Kind() != reflect.Struct {
-		return json.Unmarshal(data, ptr)
-	}
-
-	typ := ind.Type()
-	dynFields := []*dynField{}
-	for i := 0; i < ind.NumField(); i++ {
-		sf := typ.Field(i)
-		field := ind.Field(i)
-
-		if !field.CanSet() {
-			continue
-		}
-
-		dynamic := sf.Tag.Get("dynamic")
-		if dynamic == "true" {
-			rawMsg := new(json.RawMessage)
-			field.Set(reflect.ValueOf(rawMsg))
-			dynFields = append(dynFields, &dynField{
-				Name:  sf.Name,
-				Value: &field,
-			})
-		}
-	}
-
-	if err := json.Unmarshal(data, ptr); err != nil {
-		return err
-	}
-
-	for _, dynField := range dynFields {
-		rawMsg := dynField.Value.Interface().(*json.RawMessage)
-		dynVal := ptr.(DynamicFielder).NewDynamicField(dynField.Name)
-		if dynVal != nil && len(*rawMsg) > 0 {
-			if err := ParseJSON([]byte(*rawMsg), dynVal); err != nil {
-				return err
-			}
-			dynField.Value.Set(reflect.ValueOf(dynVal))
-		} else {
-			dynField.Value.Set(reflect.Zero(dynField.Value.Type())) // for json:",omitempty"
-		}
-	}
-
-	return nil
+	return dynamicjson.Parse(data, ptr)
 }
